67113a44f85ab17e87317f98ac380cc285e8f956
Fredwin Rosales <fredwinrosales@gmail.com>
2017-07-04 09:16:57 -0400

    Inicia configuración de rutas

--- config/web.php
+++ config/web.php
@@ -1,7 +1,7 @@
 <?php
 
 $params = require(__DIR__ . '/params.php');
-$db = require(__DIR__ . '/db.php');
+$url_manager = require(__DIR__ . '/url_manager.php');
 
 $config = [
     'id' => 'basic',
@@ -45,13 +45,7 @@ $config = [
             'password' => '',
             'charset' => 'utf8',
         ],
-        //'db' => $db,
-        'urlManager' => [
-            'enablePrettyUrl' => true,
-            'showScriptName' => false,
-            'rules' => [
-            ],
-        ],
+        'urlManager' => $url_manager,
     ],
     'params' => $params,
 ];
