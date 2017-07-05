7f8823a2fc5da8d7d496262ebd4ccf38d241e51f
Fredwin Rosales <fredwinrosales@gmail.com>
2017-07-05 19:47:40 -0400

    CRUD con gii
    Se crea el CRUD de la entidad maestros con el uso de la herramienta gii propia de Yii2 Framework

---
+++ views/maestro/create.php
@@ -0,0 +1,21 @@
+<?php
+
+use yii\helpers\Html;
+
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Maestros */
+
+$this->title = 'Create Maestros';
+$this->params['breadcrumbs'][] = ['label' => 'Maestros', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="maestros-create">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <?= $this->render('_form', [
+        'model' => $model,
+    ]) ?>
+
+</div>
