8c760376947a66ec981c3361d7bc036fa8db6989
Fredwin Rosales <fredwinrosales@gmail.com>
2017-07-10 22:13:37 -0400

    Login desde base de datos
    Se implementa login dinámico desde tabla users

---
+++ models/CustomUsers.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+use yii\web\IdentityInterface;
+
+/**
+ * This is the model class for table "users".
+ *
+ * @property integer $id
+ * @property string $username
+ * @property string $password
+ * @property string $auth_key
+ * @property string $password_reset_token
+ */
+class CustomUsers extends \yii\db\ActiveRecord implements \yii\web\IdentityInterface
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'users';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['username', 'password', 'auth_key', 'password_reset_token'], 'required'],
+            [['username', 'password'], 'string', 'max' => 45],
+            [['auth_key'], 'string', 'max' => 32],
+            [['password_reset_token'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'username' => 'Username',
+            'password' => 'Password',
+            'auth_key' => 'Auth Key',
+            'password_reset_token' => 'Password Reset Token',
+        ];
+    }
+
+    public static function findIdentity($id)
+    {
+        return self::findOne($id);
+    }
+
+    public static function findIdentityByAccessToken($token, $type = null)
+    {
+        throw new \yii\base\NotSupportedException();
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getAuthKey()
+    {
+        return $this->auth_key;
+    }
+
+    public function validateAuthKey($authKey)
+    {
+        return $this->auth_key === $authKey;
+    }
+
+    public static function findByUsername($username)
+    {
+        return self::findOne(['username' => $username]);
+    }
+
+    public function validatePassword($password)
+    {
+        return $this->password === $password;
+    }
+
+}
