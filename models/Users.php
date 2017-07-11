58fbc8140af6c6fd55de564bbfef4f708e959ef6
Fredwin Rosales <fredwinrosales@gmail.com>
2017-07-10 20:13:01 -0400

    CRUD User
    Se crea Modelo y CRUD para la entidad User desde el gestor gii propio de Yii2

---
+++ models/Users.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "users".
+ *
+ * @property integer $id
+ * @property string $username
+ * @property string $email
+ * @property string $password
+ */
+class Users extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'users';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['username', 'email', 'password'], 'required'],
+            [['username', 'email'], 'string', 'max' => 255],
+            [['password'], 'string', 'max' => 100],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'username' => 'Username',
+            'email' => 'Email',
+            'password' => 'Password',
+        ];
+    }
+}
